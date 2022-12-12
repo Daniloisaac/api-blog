9b823444f4b671d6ff32b53f0ad88d812deb98a4
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-12 14:19:55 -0300

    feat: criei nova função no controller que busca todas as categorias

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -3,8 +3,14 @@ const categoriesService = require('../services/categories.service');
 const createdNewCategorie = async (req, res) => {
   const { name } = req.body;
   const message = await categoriesService.insertNewCategorie(name);
-  if (message.messageError) return res.status(message.status).send({ message: message.messageError });
+if (message.messageError) return res.status(message.status).send({ message: message.messageError });
 return res.status(201).send(message);
 };
 
-module.exports = { createdNewCategorie };
+const getAllController = async (_req, res) => {
+  const message = await categoriesService.getAllService();
+if (message.messageError) return res.status(message.status).send({ message: message.messageError });
+return res.status(200).send(message);
+};
+
+module.exports = { createdNewCategorie, getAllController };
