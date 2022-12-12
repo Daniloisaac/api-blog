dd01fe219e0340a18e610b37b701986104b4647b
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-12 14:18:28 -0300

    feat: criei nova função no service que busca todas as categorias

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -3,17 +3,21 @@ const categoriesModel = require('../models');
 const insertNewCategorie = async (newCategory) => {
 console.log('** RESULTADO DO newCategory CATEGORIE **', newCategory);
   if (!newCategory) {
-console.log('** RWE FSDFSDFSD DIM **');
-
     return { status: 400, messageError: '"name" is required' };
   }
-console.log('** RWE FSDFSDFSD DINGGM **');
-
 const createdNewCategorie = await categoriesModel.Category.create({
 name: newCategory,
 });
-console.log('** RESULTADO DO INSERT CATEGORIE **', createdNewCategorie);
 return createdNewCategorie;
 };
 
-module.exports = { insertNewCategorie };
+const getAllService = async () => {
+const getAll = await categoriesModel.Category.findAll();
+console.log('** RESULTADO DO getAll CATEGORIE **', getAll);
+return getAll; 
+};
+
+module.exports = { 
+  insertNewCategorie,
+  getAllService,
+ };
