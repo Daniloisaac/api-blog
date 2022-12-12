12bf85155237e9145f8914960d64f02fd3266302
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-12 14:20:56 -0300

    feat: criei rota get categories

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -4,6 +4,7 @@ const useRouter = Router();
 const categoriesController = require('../controllers/categories.controller');
 const JWTValidate = require('../middleware/JWTValidate');
 
+useRouter.get('/', JWTValidate, categoriesController.getAllController);
 useRouter.post('/', JWTValidate, categoriesController.createdNewCategorie);
 
 module.exports = useRouter;
