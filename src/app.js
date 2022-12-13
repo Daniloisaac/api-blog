6b5ed586dcf019e46c8bda07fa5b62783cbe39ef
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-13 16:05:01 -0300

    feat: adicionei rota post no app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const login = require('./routes/routes.login');
 const user = require('./routes/user.routes');
 const categories = require('./routes/categories.routes');
+const post = require('./routes/posts.routes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/login', login);
 app.use('/user', user);
 app.use('/categories', categories);
+app.use('/post', post);
 
 // ...
 
