ea1760a611e3535027415a7309d657cce49416a2
Danilo Isaac <daniloisaac942@gmail.com>
2022-12-13 15:07:30 -0300

    fix: ajustei nome da propriedades do PostCategory

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -2,8 +2,8 @@
 
 module.exports = (sequelize, DataTypes) => {
  const PostCategory = sequelize.define('PostCategory', {
-    post_id: DataTypes.INTEGER,
-    category_id: DataTypes.INTEGER
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
   }, {
     sequelize,
     tableName: 'posts_categories',
